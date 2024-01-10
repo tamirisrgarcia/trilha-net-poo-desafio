1ff93895c5ecaad21624aaa728c23e642baf94ef
Tamiris <tamirisrgarcia@gmail.com>
2024-01-09 23:13:02 -0300

    resolucao desafio trilha .net poo

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia nokia = new Nokia("9999-8888", "1100", "1234567890", 32);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("whatsapp");
+
+
+Iphone iphone = new Iphone("1111-5555", "12", "0987654321", 128);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("telegram");
